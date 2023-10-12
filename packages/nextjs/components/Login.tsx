651e279f069208fd5f2b009549818c273203295c
Arikabz <arturo.guza1999@gmail.com>
2023-10-12 02:20:38 -0600

    changed button on login

--- packages/nextjs/components/Login.tsx
+++ packages/nextjs/components/Login.tsx
@@ -7,7 +7,10 @@ function Login() {
   return (
     <div className="bg-[#11A37F] h-screen flex flex-col items-center justify-center text-centerb">
       <Image src="https://links.papareact.com/2i6" height={400} width={400} alt="logo" />
-      <button onClick={() => signIn("google")} className="text-white font-bold text-3xl animate-pulse">
+      <button
+        onClick={() => signIn("google")}
+        className="bg-green-800 py-4 px-6 rounded-full text-white font-bold text-3xl animate-pulse"
+      >
         Sign in to use ChatGPT
       </button>
     </div>
